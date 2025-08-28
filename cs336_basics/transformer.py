9c05f1299f761099ad2ef9d51ecb6767431057d0
Grzegorz Szpak <gszpak01@gmail.com>
2025-08-28 14:10:26 +0200

    rmsnorm

--- cs336_basics/transformer.py
+++ cs336_basics/transformer.py
@@ -1,7 +1,7 @@
 import einops
 import numpy
 import torch
-
+from jaxtyping import Float
 
 class Linear(torch.nn.Module):
     def __init__(self, in_features, out_features, device=None, dtype=None):
@@ -16,7 +16,6 @@ class Linear(torch.nn.Module):
 
 
 class Embedding(torch.nn.Module):
-
     def __init__(self, num_embeddings, embedding_dim, device=None, dtype=None):
         super().__init__()
         embeddings = torch.zeros((num_embeddings, embedding_dim), device=device, dtype=dtype)
@@ -25,3 +24,26 @@ class Embedding(torch.nn.Module):
 
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
         return self.embeddings[token_ids]
+
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype=None):
+        super().__init__()
+        self.eps = eps
+        gains = torch.ones(d_model, device=device, dtype=dtype)
+        self.gains = torch.nn.Parameter(data=gains)
+        self.d_model = d_model
+
+    def forward(
+            self,
+            x: Float[torch.Tensor, "batch_size sequence_length d_model"]
+        ) -> Float[torch.Tensor, "batch_size sequence_length d_model"]:
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        rms = torch.sqrt(
+            (einops.reduce(x ** 2, '... d_model -> ...', 'sum') + torch.full(x.shape[:2], self.eps))
+            /
+            self.d_model
+        )
+        result = (x * self.gains) / einops.rearrange(rms, '... -> ... 1')
+        return result.to(in_dtype)
--- tests/adapters.py
+++ tests/adapters.py
@@ -11,7 +11,7 @@ import torch
 from torch import Tensor
 
 from cs336_basics.bpe import train_bpe
-from cs336_basics.transformer import Embedding, Linear
+from cs336_basics.transformer import Embedding, Linear, RMSNorm
 
 
 
@@ -385,7 +385,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    rms_norm = RMSNorm(d_model, eps)
+    rms_norm.gains = torch.nn.Parameter(data=weights)
+    return rms_norm.forward(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
